91d022d9a9f23b35e49648dd34dd50ff14d82df1
Impulsiona Aí <ia7609105@gmail.com>
2025-03-10 20:02:27 -0300

    implement swagger

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,18 @@
 import { Elysia } from 'elysia';
 import { cors } from '@elysiajs/cors';
+import { swagger } from '@elysiajs/swagger';
 
 const app = new Elysia({ prefix: '/api'})
-  .use(cors());
+  .use(cors())
+  .use(swagger({
+    path: '/swagger',
+    documentation: {
+      info: {
+        title: 'Impulsiona Aí',
+        version: '1.0.0',
+      },
+    },
+  }));
 
 app.listen(process.env.PORT!, () => {
   console.log('HTTP server running!');
