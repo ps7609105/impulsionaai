f65086f1a6f33d4e5dd235da728b65966dd27ea5
Impulsiona Aí <ia7609105@gmail.com>
2025-03-10 20:00:46 -0300

    implement cors

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,8 @@
 import { Elysia } from 'elysia';
+import { cors } from '@elysiajs/cors';
 
-const app = new Elysia({ prefix: '/api'});
+const app = new Elysia({ prefix: '/api'})
+  .use(cors());
 
 app.listen(process.env.PORT!, () => {
   console.log('HTTP server running!');
